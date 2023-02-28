83ec1c05182d23384cdf1f932bc18b25e7ff293e
luc_dickerson <ldickerson@basecampcodingacademy.org>
2023-02-27 21:56:06 -0600

    added todo in forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from django.forms import ModelForm
 from app.models import Tire
 
-
+#TODO
+#EXCLUDE UNNECESSARY FIELDS FOR THE FORM
 class TireForm(ModelForm):
     class Meta:
         model = Tire
