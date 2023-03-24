6296c116592146b2f475f36da2d0940aed9c0ddb
TylerA9 <tylerallen545@gmail.com>
2023-03-23 20:18:50 -0500

    Login is now working

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.forms import TireForm
 from app.models import *
 from app import models
 from .decorators import unauthenticated_user
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 # Create your views here.
 
@@ -204,8 +206,22 @@ def registerView(request):
 
 
 def loginView(request):
-    context = {}
-    return render(request, "login.html", context)
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    form = AuthenticationForm()
+    return render(request=request, template_name="login.html", context={"form": form})
 
 
 # =======EXTRA FUNCTIONS AND CHECKS=======#
