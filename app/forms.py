897c6043a21de1012186dc3f567d7fc679265017
luc_dickerson <ldickerson@basecampcodingacademy.org>
2023-03-26 20:54:35 -0500

    added store choices to userCreationForm

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from app.models import Tire
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 # TODO
 # EXCLUDE UNNECESSARY FIELDS FOR THE FORM
@@ -12,6 +13,13 @@ class TireForm(ModelForm):
 
 
 class CreateUserForm(UserCreationForm):
+    STORECHOICES = [
+        ("Southern Tire Mart", "Southern Tire Mart"),
+        ("Floyd's Tire", "Floyd's Tire"),
+        ("Walmart Tire Center", "Walmart Tire Center"),
+    ]
+    stores = forms.ChoiceField(choices=STORECHOICES, required=True)
+
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
